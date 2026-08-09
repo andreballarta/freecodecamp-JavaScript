6e05f14bfddb9deac1074fb9855519a96bcd7475
André Ballarta <ahballarta@gmail.com>
2026-08-09 03:57:02 -0500

    Add sentence maker with two stories

---
+++ sentence-maker/sentence-maker.js
@@ -0,0 +1,16 @@
+let adjective = "strange";
+let noun = "cat";
+let verb = "touched";
+let place = "skyscraper";
+let adjective2 = "tiny";
+let noun2 = "pizza";
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+adjective = "funny";
+noun = "dog";
+verb = "bored";
+place = "school";
+adjective2 = "huge";
+noun2 = "tuna";
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
