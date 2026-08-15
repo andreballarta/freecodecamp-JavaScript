89ca52f52c23da2263faa2b142330ac0dda996ee
André Ballarta <ahballarta@gmail.com>
2026-08-15 04:38:08 -0500

    Implement Celsius to Fahrenheit conversion function

---
+++ celsius-to-fahrenheit-converter/celsius_to_fahrenheit_converter.js
@@ -0,0 +1,9 @@
+function convertCtoF(temperature_C){
+  return temperature_C * (9/5) + 32;
+}
+
+console.log(convertCtoF(-30));
+console.log(convertCtoF(-10));
+console.log(convertCtoF(0));
+console.log(convertCtoF(20));
+console.log(convertCtoF(30));
