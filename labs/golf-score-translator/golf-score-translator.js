c15b038c550524469b85fd735550964ea9396478
André Ballarta <ahballarta@gmail.com>
2026-08-20 13:56:13 -0500

    Add golfScore function to translate golf scores
    Implement golf score translator function with examples.

---
+++ labs/golf-score-translator/golf-score-translator.js
@@ -0,0 +1,34 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  if (strokes === 1) {
+    return names[0];
+  }
+  if (strokes <= (par - 2)) {
+    return names[1];
+  }
+    if (strokes <= (par - 1)) {
+    return names[2];
+  }
+    if (strokes === par) {
+    return names[3];
+  }
+    if (strokes === (par + 1)) {
+    return names[4];
+  }
+    if (strokes === (par + 2)) {
+    return names[5];
+  }
+    if (strokes >= (par + 3)) {
+    return names[6];
+  }
+}
+
+// examples of usage
+console.log(golfScore(3, 1));
+console.log(golfScore(5, 2));
+console.log(golfScore(3, 2));
+console.log(golfScore(3, 3));
+console.log(golfScore(4, 5));
+console.log(golfScore(3, 5));
+console.log(golfScore(4, 8));
